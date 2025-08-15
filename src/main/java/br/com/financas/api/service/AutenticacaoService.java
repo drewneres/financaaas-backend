4fc0ef33f67b90315d0f7f20da7e00414369a20d
drewneres <hendrewneresfsa@hotmail.com>
2025-08-14 22:42:36 -0300

    feat: Implementa AutenticacaoService com lógica de busca de usuário

--- src/main/java/br/com/financas/api/service/AutenticacaoService.java
+++ src/main/java/br/com/financas/api/service/AutenticacaoService.java
@@ -1,4 +1,21 @@
 package br.com.financas.api.service;
 
-public class AutenticacaoService {
+import br.com.financas.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service // Marca esta classe como um serviço do Spring
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Esta é a consulta que o Spring Security fará para buscar o usuário no banco
+        return repository.findByLogin(username);
+    }
 }
